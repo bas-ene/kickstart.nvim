6f7771669bbd365c2fcbfc96c886f9aef7f9c9dc
bas-ene <enea.basilico@gmail.com>
2023-12-11 10:18:07 +0100

    spostata roba

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,16 +3,16 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-	require("custom.plugins.lualine"),
-	require("custom.plugins.autopairs"),
-	require("custom.plugins.ntree"),
-	require("custom.plugins.toggleterm"),
-	require("custom.plugins.neogit"),
-	require("custom.plugins.indent"),
-	require("custom.plugins.scroller"),
-	require("custom.plugins.autotag"),
-	require("custom.plugins.TODO"),
-	require("custom.plugins.notify"),
+	require("custom.plugins.plugins.lualine"),
+	require("custom.plugins.plugins.autopairs"),
+	require("custom.plugins.plugins.ntree"),
+	require("custom.plugins.plugins.toggletermtoggleterm"),
+	require("custom.plugins.plugins.neogit"),
+	require("custom.plugins.plugins.indent"),
+	require("custom.plugins.plugins.scroller"),
+	require("custom.plugins.plugins.autotag"),
+	require("custom.plugins.plugins.TODO"),
+	require("custom.plugins.plugins.notify"),
 }
 
 -- TODO frmf
